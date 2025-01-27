b04e5e762e9c5ab9095c4090122fe8ef3e9edd0d
Erick Carbajal <86636258+Thekiller2001@users.noreply.github.com>
2025-01-26 23:25:28 -0500

    Se agrega al GitHub el proyecto desarrollado

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-console.log("Ejecutando el sistema")
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+let longitudLista = 0;
+
+console.log(listaAmigos);
+
+function limparCaja(){
+    //let valorCaja = document.querySelector("#valorUsuario");
+    //valorCaja.value = "";
+
+    valorCaja = document.querySelector("#amigo").value = "";
+};
+
+function sortearAmigo(){
+    let amigoRandom = Math.floor(Math.random()*listaAmigos.length);
+    if (amigoRandom == -1){
+        return amigoSecreto;
+    } else{
+        console.log(amigoRandom)
+        console.log(listaAmigos[amigoRandom]);
+        let muestraamigoSecreto = document.querySelector("#resultado");
+        muestraamigoSecreto.innerHTML = listaAmigos[amigoRandom];
+        }
+};
+
+
+function mostrarAmigos(){
+    let muestraLista = document.querySelector("#listaAmigos");
+    muestraLista.innerHTML = listaAmigos.join("<br>");
+}
+
+function agregarAmigo(){
+    let amigoAgregado = document.getElementById("amigo").value;
+    listaAmigos.push(amigoAgregado);
+    longitudLista = listaAmigos.length;
+    console.log(longitudLista);
+    console.log(amigoAgregado);
+    console.log(listaAmigos);
+    limparCaja();
+    mostrarAmigos();
+};
+
+
